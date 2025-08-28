f9eed384a25406882afaa4f61094e834ddc9fdca
Uislan Cruz <uislansk8@hotmail.com>
2025-08-27 23:27:04 -0300

    Implementando as classes

--- src/com/algaworks/banco/Conta.java
+++ src/com/algaworks/banco/Conta.java
@@ -1,4 +1,41 @@
 package com.algaworks.banco;
 
 public class Conta {
+
+    private Titular titular;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
