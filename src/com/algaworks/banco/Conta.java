7918aeb86443ef6b824a65880eb0b938f6670bfe
Uislan Cruz <uislansk8@hotmail.com>
2025-09-29 22:19:22 -0300

    Colocando final no metodo depositar, para impedir alterações que impacte no programa

--- src/com/algaworks/banco/Conta.java
+++ src/com/algaworks/banco/Conta.java
@@ -47,7 +47,7 @@ public class Conta {
         saldo -= valorSaque;
     }
 
-    public void depositar(double valorDeposito) {
+    public final void depositar(double valorDeposito) {
         if (valorDeposito <= 0) {
             throw new IllegalArgumentException("Valor do depósito deve ser maior que 0");
         }
